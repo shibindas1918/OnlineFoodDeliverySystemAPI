b33058ee109b3ee96ed61605ad12c32b5759a680
sibin <sibindas1806@gmail.com>
2024-11-21 17:37:36 +0530

    swagger issue fixed

--- OnlineFoodDeliverySystemAPI/Program.cs
+++ OnlineFoodDeliverySystemAPI/Program.cs
@@ -2,34 +2,39 @@ using OnlineFoodDeliverySystemAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton(new DatabaseHelper(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-builder = WebApplication.CreateBuilder(args);
-
+// Add services to the container
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
     });
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Online Food Delivery System API",
+        Version = "v1"
+    });
+});
+
+// Add DatabaseHelper as a singleton
+builder.Services.AddSingleton(sp =>
+    new DatabaseHelper(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Online Food Delivery System API v1"));
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
