ef40e909a0587a9eb3de5e64409c8dbd402ea5dd
sibin <sibindas1806@gmail.com>
2024-11-21 14:39:49 +0530

    DatabaseHelper  Implementation

---
+++ OnlineFoodDeliverySystemAPI/Data/DatabaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace OnlineFoodDeliverySystemAPI.Data
+{
+    public class DatabaseHelper
+    {
+        private readonly string _ConnectionString;
+        public DatabaseHelper(string connectionString)
+        {
+            _ConnectionString = connectionString;
+        }
+        public DataTable ExecuteQuery(string query)
+        {
+            using (SqlConnection conn = new SqlConnection(_ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        var datable = new DataTable();
+                        adapter.Fill(datable);
+                        return datable;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
