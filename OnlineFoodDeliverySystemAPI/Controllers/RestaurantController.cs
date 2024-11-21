811c5ee0620a58b9ac88618ab36306f94ee4ec94
sibin <sibindas1806@gmail.com>
2024-11-21 15:36:37 +0530

    All Restaurants get controller method

---
+++ OnlineFoodDeliverySystemAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineFoodDeliverySystemAPI.Data;
+
+namespace OnlineFoodDeliverySystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly DatabaseHelper _databaseHelper;
+        public RestaurantController(DatabaseHelper databaseHelper)
+        {
+            _databaseHelper = databaseHelper;
+        }
+        [HttpGet("ALL-Restaurants")]
+        public IActionResult Get()
+        {
+            string query = "select *from Restaurants";
+            try
+            {
+                var result = _databaseHelper.ExecuteQuery(query);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
--- OnlineFoodDeliverySystemAPI/Program.cs
+++ OnlineFoodDeliverySystemAPI/Program.cs
@@ -1,3 +1,5 @@
+using OnlineFoodDeliverySystemAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(new DatabaseHelper(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
